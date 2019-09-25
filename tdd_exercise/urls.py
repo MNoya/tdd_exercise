fff5ae3ee8caf17a9c34cbd9837bfeaada399855
Martin Noya <martinnoya@gmail.com>
2019-09-25 09:20:02 -0300

    Added urls and minimal views for drill and answer

--- tdd_exercise/urls.py
+++ tdd_exercise/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from xword_data import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.drill, name='xword-drill'),
+    path('answer/<int:clue_id>', views.answer, name='xword-answer'),
+
 ]
--- xword_data/views.py
+++ xword_data/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def drill(request):
+    return render(request, 'drill.html', {})
+
+
+def answer(request, clue_id):
+    return render(request, 'answer.html', {})
