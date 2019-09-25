2414d1c5de91a329801ad8f710ce5b25e51c7166
Martin Noya <martinnoya@gmail.com>
2019-09-25 09:04:04 -0300

    Updated str repr of models
    TestXWordModels passed

--- xword_data/models.py
+++ xword_data/models.py
@@ -7,9 +7,15 @@ class Puzzle(models.Model):
     byline = models.CharField(max_length=255)
     publisher = models.CharField(max_length=12)
 
+    def __str__(self):
+        return f'{self.publisher} {self.date}'
+
 
 class Entry(models.Model):
     entry_text = models.CharField(unique=True, max_length=50)
+
+    def __str__(self):
+        return f'{self.entry_text}'
 
 
 class Clue(models.Model):
@@ -17,3 +23,6 @@ class Clue(models.Model):
     puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
     clue_text = models.CharField(max_length=512)
     theme = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.entry} {self.clue_text}'
