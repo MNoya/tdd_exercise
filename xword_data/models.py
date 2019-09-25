436c6c0e814895592d062e76e4a3dffe2826e77d
Martin Noya <martinnoya@gmail.com>
2019-09-25 08:57:52 -0300

    Added models
    With associated migration file

--- xword_data/models.py
+++ xword_data/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Puzzle(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    date = models.DateField()
+    byline = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=12)
+
+
+class Entry(models.Model):
+    entry_text = models.CharField(unique=True, max_length=50)
+
+
+class Clue(models.Model):
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+    clue_text = models.CharField(max_length=512)
+    theme = models.BooleanField(default=False)
