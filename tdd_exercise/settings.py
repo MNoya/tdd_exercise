a334888c3c05e5bb122fb21ddbad9bbf9e7c2818
Martin Noya <martinnoya@gmail.com>
2019-09-25 10:28:42 -0300

    Updated FIXTURE_DIRS to include root-level fixtures directory

--- tdd_exercise/settings.py
+++ tdd_exercise/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tdd_exercise', 'static')]
+
+FIXTURE_DIRS = [os.path.join(BASE_DIR, 'fixtures')]
